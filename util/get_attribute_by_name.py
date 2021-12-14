f184be9000e2a1c31ab54a5d847e1e15f81aed9c
Ningchuan Xiao <ncxiao@gmail.com>
2021-12-14 01:43:45 -0500

    new

--- util/get_attribute_by_name.py
+++
@@ -1,8 +0,0 @@
-def get_shp_attribute_by_name(shp, attrname):
-    """
-    shp: shapex object
-    """
-    val = []
-    for f in shp:
-        val.append(f['properties'][attrname])
-    return val
